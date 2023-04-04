f4e8a2228d6be61fea33e17a7b1251903b677322
Deividas <deividasstankunas9@gmail.com>
2023-04-04 18:02:34 +0300

    uigi

--- nuotaika.js
+++ nuotaika.js
@@ -1,6 +1,6 @@
 $(window).on('scroll', function() {
     var scrollTop = $(this).scrollTop();
-    if (scrollTop > 200) { // adjust the number to the scroll position where you want the text to appear
+    if (scrollTop > 20000) { // adjust the number to the scroll position where you want the text to appear
       $('.spinner-border.g-item').addClass('show');
     } else {
       $('.spinner-border.g-item').removeClass('show');

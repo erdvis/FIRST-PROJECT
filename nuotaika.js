a9c4fc441775b6711088ca6777e41d0f7ef02669
Deividas <deividasstankunas9@gmail.com>
2023-04-05 16:06:36 +0300

    sdfsdfssd

--- nuotaika.js
+++ nuotaika.js
@@ -1,9 +1,30 @@
-$(window).on('scroll', function() {
-    var scrollTop = $(this).scrollTop();
-    if (scrollTop > 20000) { // adjust the number to the scroll position where you want the text to appear
-      $('.spinner-border.g-item').addClass('show');
-    } else {
-      $('.spinner-border.g-item').removeClass('show');
-    }
+// Replace ACCESS_TOKEN with your Strava access token
+const ACCESS_TOKEN = "c3a58af0fd13e83ab1166609561afd27be33ace1";
+
+// Construct the API endpoint URL
+const API_ENDPOINT = "https://www.strava.com/api/v3/athlete/activities";
+
+// Set up the API request headers
+const headers = {
+  Authorization: `Bearer ${ACCESS_TOKEN}`,
+};
+
+// Make the API request using fetch()
+fetch(API_ENDPOINT, { headers })
+  .then((response) => response.json())
+  .then((activities) => {
+    // Loop through the retrieved activities and add them to the list
+    activities.forEach((activity) => {
+      const activityElement = document.createElement("li");
+      activityElement.classList.add("activity");
+      activityElement.innerHTML = `
+        <h2>${activity.name}</h2>
+        <p>${activity.distance / 1000} km</p>
+      `;
+      document.getElementById("activities").appendChild(activityElement);
+    });
+  })
+  .catch((error) => {
+    // Handle any errors that occur during the API request
+    console.error(error);
   });
-  
